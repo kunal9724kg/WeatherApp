f65ae2a6598d699c483488553c17453da161b279
Kunal Gupta <kunal9724kg@Kunals-MacBook-Air.local>
2020-04-25 00:55:26 +0530

    Fixed the error of writing HTML content into res.write()

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const https = require("https")
 server.get("/", function(req, res){
   const url="https://api.openweathermap.org/data/2.5/weather?appid=1eb1071bd5782b657a71c5aa210f49d9&q=kolkata&units=metric"
   https.get(url, function(response){
+
     response.on("data", function(data){
+      res.writeHead(200,{"Content-Type" : "text/html"});
       const weatherData = JSON.parse(data)
       const weatherDescription=weatherData.weather[0].description
       const temperature= weatherData.main.temp
       const icon=weatherData.weather[0].icon
       const imageURL= "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.send("<img src = " + imageURL + ">")
-
+      res.write("<img src = " + imageURL + ">")
+      res.send()
 
     })
 
